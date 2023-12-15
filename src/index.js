c407172a1e4b59c8a6cef423e5331131d876fd5b
MIchelData <uebelhartcalvin139@gmail.com>
2023-12-15 11:05:36 +0100

    add api connexion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import css from "./css/main.css";
+import css from './css/main.css';
 
-import * as map from "./objects/Map"
+import * as map from './objects/Map';
 
-import * as rangeInput from "./objects/DataInputs/rangeInput"
+import * as rangeInput from './objects/DataInputs/rangeInput';
 
-import * as d3 from "d3";
+import * as d3 from 'd3';
 import { json } from 'd3-fetch';
-    
 
+import connection from './utils/api';
+
+connection();
